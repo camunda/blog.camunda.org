5e14114d08e964b65fc23b0cf14ff0b826dcf8b4
Valentin Vago <zeropaper@irata.ch>
2015-12-02 14:15:17 +0100

    feat(lightbox): add lightbox on images
    in order to be able to see images in their original size
    
    Related to CAM-5090

--- theme-src/scripts/img-lazy-loading.js
+++ theme-src/scripts/img-lazy-loading.js
@@ -5,6 +5,41 @@
 'use strict';
 /*global require: false*/
 var utils = require('./utils');
+var query = utils.query;
+var attr = utils.attr;
+var mkEl = utils.mkEl;
+
+var lightbox = mkEl('div', {'class': 'lightbox'});
+var lightboxContent = mkEl('div', {'class': 'content'});
+var lightboxImg = mkEl('img');
+lightbox.appendChild(lightboxContent);
+lightbox.addEventListener('click', function () {
+  lightbox.classList.remove('open');
+});
+lightboxContent.appendChild(lightboxImg);
+document.body.appendChild(lightbox);
+
+
+function showBigger(evt) {
+  var img = evt.target.src ? evt.target : query('img', evt.target);
+  attr(lightboxImg, 'src', img.src);
+  var style = lightboxContent.style;
+  if (img.naturalWidth < document.body.clientWidth) {
+    style.marginLeft = (-4 - (img.naturalWidth / 2)) + 'px';
+  }
+  else {
+    style.marginLeft = (0 - (document.body.clientWidth / 2)) + 'px';
+  }
+
+  if (img.naturalHeight < document.body.clientHeight) {
+    style.marginTop = (-4 - (img.naturalHeight / 2)) + 'px';
+  }
+  else {
+    style.marginTop = (0 - (document.body.clientHeight / 2)) + 'px';
+  }
+
+  lightbox.classList.add('open');
+}
 
 
 function lazyLoadImages() {
@@ -13,16 +48,24 @@ function lazyLoadImages() {
   images.forEach(function (item) {
     if (utils.isElementInViewport(item)) {
       var wrap = item.parentNode;
-      console.info('loading');
       if (wrap) wrap.classList.add('loading');
+
       item.addEventListener('load', function () {
-        console.info('loaded');
         if (wrap) wrap.classList.remove('loading');
+
+        // ensure lightbox when needed
+        var figure = item.parentNode.parentNode;
+        if (item.clientWidth < item.naturalWidth && !figure.classList.contains('no-border')) {
+          figure.classList.add('clickable');
+          figure.addEventListener('click', showBigger);
+        }
       });
+
       item.setAttribute('src', item.getAttribute('data-src'));
       item.removeAttribute('data-src');
     }
   });
+
   // if all the images are loaded, stop calling the handler
   if (images.length === 0) {
     window.removeEventListener('DOMContentLoaded', lazyLoadImages);
@@ -32,7 +75,7 @@ function lazyLoadImages() {
   }
 }
 
-//these handlers will be removed once the images have loaded
+// these handlers will be removed once the images have loaded
 window.addEventListener('DOMContentLoaded', lazyLoadImages);
 window.addEventListener('load', lazyLoadImages);
 window.addEventListener('resize', lazyLoadImages);
--- theme-src/scripts/utils.js
+++ theme-src/scripts/utils.js
@@ -67,3 +67,15 @@ function isElementInViewport(el) {
   );
 }
 utils.isElementInViewport = isElementInViewport;
+
+var _init = [];
+function docLoaded(fn) { _init.push(fn); }
+document.addEventListener('readystatechange', function () {
+  if (document.readyState === 'complete') {
+    var fn;
+    /*jshint boss: true*/
+    while (fn = _init.shift()) { fn(); }
+    /*jshint boss: false*/
+  }
+});
+utils.docLoaded = docLoaded;
